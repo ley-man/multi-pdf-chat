62f78ab5a6cddfe92eb396619882ec88e3e684ad
ley-man <pkumar14@stevens.edu>
2023-12-04 18:44:25 -0500

    extract text from multiple pdfs

--- app.py
+++ app.py
@@ -1,17 +1,57 @@
 import streamlit as st
+from dotenv import load_dotenv  
+import os
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfFileReader
+import PyPDF2
 
+
+def get_pdf_text(pdf_docs):
+    pdf_text = ""
+    for pdf_doc in pdf_docs:
+      with open(pdf_doc, 'rb') as f:
+          pdf = PyPDF2.PdfFileReader(f)
+          for page in range(pdf.getNumPages()):
+              page_text = pdf.getPage(page).extractText()
+              pdf_text += page_text
+      return pdf_text
 
 def main():
+  load_dotenv()
+
   st.set_page_config(page_title="Multi-pdf-chat", page_icon=":sharks:")
 
   st.subheader("Chat with multiple pdf documents :sunglasses:")  
   st.text_input("Ask a question regarding your docs")
 
   with st.sidebar:
+     
      st.subheader ("Please upload your docs")  
-     st.file_uploader("Click Process after uploading", type=["pdf"]
+     pdf_docs = st.file_uploader("Click Process after uploading", type=["pdf"]
                       ,accept_multiple_files=True)
-     st.button("Process")
+     if st.button("Process"):
+        with st.spinner("Processing..."):
+            for pdf_doc in pdf_docs:
+               print (pdf_doc)
+                # get pdf text
+                # for pdf_doc in pdf
+
+                # divide in chunks
+
+                # create embeddings
+
+     #  Markdown
+     add_vertical_space(num_lines=3)
+     st.markdown('''
+     ## About
+     This app is designed to chat with your personal pdf articles and books.
+     Tools used:
+     - [Streamlit](https://streamlit.io/)
+     - [Langchain](https://python.langchain.com/)
+     - [OpenAI](https://platform.openai.com/)
+     - [Llama2](https://ai.meta.com/llama/)
+     ''')
+     
 
 
 
